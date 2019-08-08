e22f223bf47c9e0cac476596d6b96b74653d6d69
Work <work@sean-ohs-mbp.lan>
2019-08-08 12:52:16 -0700

    Style app bar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    flexGrow: 1
+  },
+  button: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  }
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
     <div className="App">
       <header className="App-header">
         <AppBar position='static'>
           <Toolbar>
-            <Button color='inherit'>Test1</Button>
-            <Button color='inherit'>Test2</Button>
-            <Button color='inherit'>Test3</Button>
+            <Typography variant='h6' className={classes.title}>
+              Quicksight Embedding Demo
+            </Typography>
+            <Button color='inherit' className={classes.button}>Test1</Button>
+            <Button color='inherit' className={classes.button}>Test2</Button>
+            <Button color='inherit' className={classes.button}>Test3</Button>
           </Toolbar>
         </AppBar>
       </header>
