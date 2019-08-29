4a69f5b914088ef93e0902f1b490fb4a15f03680
Sean Oh <sean@buildops.com>
2019-08-28 17:37:03 -0700

    Updates

---
+++ precache-manifest.6272c309316aca2ccae1a23059b89b62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a072cd1dbb1be2ab77c62af0875be916",
+    "url": "/quicksight-demo/index.html"
+  },
+  {
+    "revision": "f519244b94bd7f4e39fe",
+    "url": "/quicksight-demo/static/css/main.6259f84f.chunk.css"
+  },
+  {
+    "revision": "35bf258d6cf388c246f7",
+    "url": "/quicksight-demo/static/js/2.c2bf7a6c.chunk.js"
+  },
+  {
+    "revision": "f519244b94bd7f4e39fe",
+    "url": "/quicksight-demo/static/js/main.06990355.chunk.js"
+  },
+  {
+    "revision": "59b1d9c93bc435b6f0fd",
+    "url": "/quicksight-demo/static/js/runtime~main.0f4839cc.js"
+  }
+]);
